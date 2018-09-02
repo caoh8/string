8984018b903c5d093ba263db5dbe8687e83ab60c
caoh8 <34362005+caoh8@users.noreply.github.com>
2018-09-02 21:19:08 +0800

    均分01

--- Equalization01/main.cpp
+++ Equalization01/main.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
-
+﻿#include <iostream>
+#include <string>
 using namespace std;
+
+int howmany(string s)
+{
+    int numL = 0;
+    int numR = 0;
+    int end = s.size()/2;
+    int rev = s.size() - 1;
+    for(int i=0; i<end; i++)
+    {
+        if(s[i] == '1')
+        {
+            numL++;
+        }
+        if(s[rev-i] == '1')
+        {
+            numR++;
+        }
+    }
+    if(numL == numR)
+        return 1;
+    return 2;
+}
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << howmany("0101001111") << endl;
     return 0;
 }
